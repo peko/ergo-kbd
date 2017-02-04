d975d5de804d4adcdb406ea27bdf9dda22e1c130
peko <gasu.basu@gmail.com>
2017-02-04 02:14:13 +0000

    update

--- xkb/space.sh
+++ xkb/space.sh
@@ -6,7 +6,7 @@ spare_modifier="Menu"
 xmodmap -e "keycode 65 = $spare_modifier"
 # make it AltGr modifier 
 xmodmap -e "remove mod4 = $spare_modifier"
-xmodmap -e "add AltGr = $spare_modifier"
+xmodmap -e "add LVL3 = $spare_modifier"
 xmodmap -e "keycode any = space"
 
 xcape -e "$spare_moifier=space"
